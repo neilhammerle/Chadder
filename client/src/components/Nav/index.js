038533740e962a6f0fbbd60c5a4ae559f3ab2a67
Neil Hammerle <neilhammerle@gmail.com>
2020-09-24 19:29:48 -0700

    adds updates on navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,30 +4,43 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container';
+import Dropdown from 'react-bootstrap/Dropdown'
 
 const index = () => {
     return (
         <div>
             <Router>
                 <Container className="p-0" fluid={true}>
-                    <Navbar className='border-bottom' bg='dark' variant='dark' expand='lg'>
+                    <Navbar className='border-bottom' bg='warning' variant='warning' expand='sm'>
                         <Navbar.Brand href="/">Chadder</Navbar.Brand>
+                        <Navbar.Collapse className='justify-content-center'>
+                            <Form inline>
+                                <Dropdown>
+                                    <Dropdown.Toggle variant='outline-dark'>
+                                        Sign In
+                                </Dropdown.Toggle>
+                                    <Dropdown.Menu>
+                                        <Form>
+                                            <Form.Group controlId="Email">
+                                                <Form.Label>Email Here</Form.Label>
+                                                <Form.Control type="email" placeholder="Enter email" />
+                                            </Form.Group>
 
-                        <Navbar.Toggle className='border-0' aria-controls='navbar-toggle' />
-                        <Navbar.Collapse id='navbar-toggle'>
-                            <Nav className='ml-auto'>
-                                <Nav.Link href='/'>Home</Nav.Link>
-                                <Nav.Link href='/About'>About</Nav.Link>
-                                <Nav.Link href='/Contact'>Contact</Nav.Link>
-                            </Nav>
+                                            <Form.Group controlId="formBasicPassword">
+                                                <Form.Label>Password</Form.Label>
+                                                <Form.Control type="password" placeholder="Password" />
+                                            </Form.Group>
+                                            <Button variant="outline-warning" type="submit">
+                                                Submit
+                                            </Button>
+                                        </Form>
+                                    </Dropdown.Menu>
+                                </Dropdown>
+                            </Form>
+
                         </Navbar.Collapse>
-
-                        <Form inline>
-                            <Button variant='outline-warning'>Sign In</Button>
-                        </Form>
                     </Navbar>
                 </Container>
             </Router>
