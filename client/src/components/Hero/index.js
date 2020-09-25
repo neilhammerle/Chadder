5bd90f7ad101c9188d6dcdd9db95838b372543f8
Neil Hammerle <neilhammerle@gmail.com>
2020-09-25 14:33:14 -0700

    add css on form

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -8,7 +8,7 @@ const index = () => {
         <section id="hero">
             <div className="container">
                 <div className="row justify-content-between">
-                    <div clasName="col-lg-3 pt-5 pt-lg-0 d-flex">
+                    <div>
                         <h1 className="text-left">See what's going on
             <br />
                             annoymously. </h1>
@@ -19,9 +19,9 @@ const index = () => {
             </h2>
                         <form>
                             <label>
-                                <input type='text' placeholder="Enter Zip Code!" name='zipcode' />
+                                <input id='zipform' type='text' placeholder="Enter Zip Code!" name='zipcode' />
                             </label>
-                            <input type="submit" value='Submit' />
+                            <input id='zipsubmit' type="submit" value='Submit' />
                         </form>
                     </div>
                     <div className=" align-items-right justify-content-right rounded-circle hero-img">
