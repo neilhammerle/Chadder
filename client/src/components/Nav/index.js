e215ea7750142653d65592c4723bb4704c494300
Neil Hammerle <neilhammerle@gmail.com>
2020-09-26 12:09:01 -0700

    add styles to navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,45 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './assets/styles.css';
 
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Dropdown from 'react-bootstrap/Dropdown'
 
 const index = () => {
     return (
         <div>
             <Router>
                 <Container className="p-0" fluid={true}>
-                    <Navbar className='border-bottom' bg='warning' variant='warning' expand='lg'>
-                        <Navbar.Brand href="/">Chadder</Navbar.Brand>
-                        <Navbar.Collapse className='justify-content-center'>
-                            <Form inline>
-                                <Dropdown>
-                                    <Dropdown.Toggle variant='outline-dark'>
-                                        Sign In
-                                </Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <Form>
-                                            <Form.Group controlId="Email">
-                                                <Form.Label>Email Here</Form.Label>
-                                                <Form.Control type="email" placeholder="Enter email" />
-                                            </Form.Group>
-
-                                            <Form.Group controlId="formBasicPassword">
-                                                <Form.Label>Password</Form.Label>
-                                                <Form.Control type="password" placeholder="Password" />
-                                            </Form.Group>
-                                            <Button variant="outline-warning" type="submit">
-                                                Submit
-                                            </Button>
-                                        </Form>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            </Form>
-
+                    <Navbar id='navbar' collapseOnSelect expand="lg" bg="warning" variant="warning">
+                        <Navbar.Brand id='navbartabs' href="/">Chadder</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="mr-auto">
+                                <Nav.Link id='navbartabs' href="/about">About</Nav.Link>
+                                <Nav.Link id='navbartabs' href="/contact">Contact</Nav.Link>
+                                <Nav.Link id='navbartabs' href="/signin">Sign In</Nav.Link>
+                                <Nav.Link id='navbartabs' href="/register">
+                                    Sign Up
+      </Nav.Link>
+                            </Nav>
                         </Navbar.Collapse>
                     </Navbar>
                 </Container>
